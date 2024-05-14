bd4419ccf4cafa169d708312ccad71632ef4a9f9
AyushGinoya <ginoyaayush@gmail.com>
2024-05-14 22:56:44 +0530

    Version 1.0

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'dart:async';
-
+// ignore: unused_import
+import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -7,7 +7,7 @@ import 'package:pet_adoption_app/helper/get_user_model.dart';
 import 'package:pet_adoption_app/helper/stripe_keys.dart';
 import 'package:pet_adoption_app/models/user_model.dart';
 import 'package:pet_adoption_app/screens/navigation_bar.dart';
-import 'package:pet_adoption_app/screens/routes/login.dart';
+import 'package:pet_adoption_app/screens/routes/register.dart';
 import 'package:uuid/uuid.dart';
 
 import 'package:flutter_stripe/flutter_stripe.dart';
@@ -18,7 +18,6 @@ void main() async {
   Stripe.publishableKey = StripeKeys.publishableKey;
   await Stripe.instance.applySettings();
   //await FirebaseAppCheck.instance.activate();
-
   try {
     await Firebase.initializeApp(
       options: const FirebaseOptions(
@@ -33,18 +32,29 @@ void main() async {
   } catch (e) {
     print("Error initializing Firebase: $e");
   }
-  runApp(MySplashApp());
+
+  User? user = FirebaseAuth.instance.currentUser;
+  if (user == null) {
+    //Logged in
+    runApp(const MyApp());
+  } else {
+    UserModel? currentModel = await GetUserModel.getUserModelById(user.uid);
+    if (currentModel != null) {
+      runApp(MyAppLoggedIn(userModel: currentModel, firebaseUser: user));
+    } else {
+      runApp(const MyApp());
+    }
+  }
 }
 
 //not loggedIn
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Login(),
+      home: Register(),
     );
   }
 }
@@ -54,7 +64,6 @@ class MyAppLoggedIn extends StatelessWidget {
   final User firebaseUser;
   const MyAppLoggedIn(
       {super.key, required this.userModel, required this.firebaseUser});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -62,66 +71,6 @@ class MyAppLoggedIn extends StatelessWidget {
       home: NavigationMenu(
         userModel: userModel,
         firebaseUser: firebaseUser,
-      ),
-    );
-  }
-}
-
-class MySplashApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: SplashScreen(),
-    );
-  }
-}
-
-class SplashScreen extends StatefulWidget {
-  @override
-  _SplashScreenState createState() => _SplashScreenState();
-}
-
-class _SplashScreenState extends State<SplashScreen> {
-  @override
-  void initState() {
-    super.initState();
-    Timer(const Duration(seconds: 3), checkAuthentication);
-  }
-
-  void checkAuthentication() async {
-    User? user = FirebaseAuth.instance.currentUser;
-    if (user == null) {
-      Navigator.of(context)
-          .pushReplacement(MaterialPageRoute(builder: (_) => const MyApp()));
-    } else {
-      UserModel? userModel = await GetUserModel.getUserModelById(user.uid);
-      if (userModel != null) {
-        Navigator.of(context).pushReplacement(MaterialPageRoute(
-            builder: (_) =>
-                MyAppLoggedIn(userModel: userModel, firebaseUser: user)));
-      } else {
-        Navigator.of(context)
-            .pushReplacement(MaterialPageRoute(builder: (_) => const MyApp()));
-      }
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              'assets/images/splash_screen.png',
-              fit: BoxFit.fitHeight,
-            ),
-            // const SizedBox(height: 20),
-            const CircularProgressIndicator(),
-          ],
-        ),
       ),
     );
   }
